fd3736245e7c23e662ee014d4b41ce75526b6b3f
nigue <anibaldavor@gmail.com>
2026-02-08 13:01:25 -0300

    feat: Add headers to ThresholdRepository

--- src/repository/ThresholdRepository.h
+++ src/repository/ThresholdRepository.h
@@ -1,32 +1,17 @@
 #pragma once
 
-#include <EEPROM.h>
+#include <stdint.h>
 #include "../model/Threshold.h"
-#include "../eeprom/EepromLayout.h"
+#include "EepromStorage.h"
 
 class ThresholdRepository {
-public:
-  static void save(const Threshold& threshold) {
-    uint8_t index = threshold.getSensorIndex();
-    if (index >= EepromLayout::THRESHOLD_COUNT) return;
 
-    EEPROM.put(
-      EepromLayout::thresholdAddr(index),
-      threshold.getValue()
-    );
-  }
+  private:
+    EepromStorage& repository_;
 
-  static Threshold load(uint8_t index) {
-    if (index >= EepromLayout::THRESHOLD_COUNT) {
-      return Threshold(index, 0);
-    }
+  public:
+    explicit ThresholdRepository(EepromStorage& repository);
 
-    int16_t value;
-    EEPROM.get(
-      EepromLayout::thresholdAddr(index),
-      value
-    );
-
-    return Threshold(index, value);
-  }
+    void save(const Threshold& threshold);
+    Threshold load(uint8_t index) const;
 };
