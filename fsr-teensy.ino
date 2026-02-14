ffaf7cf6c6637746edc5baefabcfd47caa7a8e84
nigue <anibaldavor@gmail.com>
2026-02-14 15:31:39 -0300

    update setup

--- fsr-teensy.ino
+++ fsr-teensy.ino
@@ -1,22 +1,16 @@
 #include "src/model/Threshold.h"
 
+#include "src/plataform/ArduinoEepromStorage.h"
+#include "src/repository/EepromHeaderRepository.h"
 
-//todo namespace
-Threshold threshold(1, 512);
+ArduinoEepromStorage storage;
+EepromHeaderRepository headerRepo(storage);
 
 void setup() {
-  //PadSide side = PAD_LEFT;
-  /*
-  PadConfigRepository::saveSide(side);
-
-  Threshold t0(0, 512);
-  Threshold t1(1, 700);
-
-  ThresholdRepository::save(t0);
-  ThresholdRepository::save(t1);
-  */
-
-  //Threshold loaded = ThresholdRepository::load(0);
+    
+  if (!headerRepo.isInitialized()) {
+    headerRepo.initialize();
+  }
 }
 
 void loop() {}
